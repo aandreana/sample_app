882055185172bb25188e6c493ac78ccc01a4e315
Tony Andreana <tony.andreana@rogers.com>
2015-04-28 01:04:19 +0000

    Finished log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
 	test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -10,6 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -21,5 +24,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
+    assert is_logged_in?
+
   end
 end
